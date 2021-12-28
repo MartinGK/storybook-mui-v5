67d2637a5e785ca1c6f5ae6f2027603ad8dbb482
MartinGK <mrtgeka@gmail.com>
2021-12-28 16:28:37 -0300

    update

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Button as MuiButton } from "@mui/material";
+// import { Button as MuiButton } from "@material-ui/core";
 
 export type ButtonProps = {
   content: string;
